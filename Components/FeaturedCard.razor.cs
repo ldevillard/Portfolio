25ec84807d5fb8e8f39f79253f5ef2304adc1ffc
ldevillard <logandvllrd@gmail.com>
2026-03-25 18:19:16 +0100

    added professional project cards

--- Components/FeaturedCard.razor.cs
+++ Components/FeaturedCard.razor.cs
@@ -9,8 +9,9 @@ public partial class FeaturedCard : ComponentBase
     [Parameter] public string Description { get; set; } = string.Empty;
     [Parameter] public string ImageSrc { get; set; } = string.Empty;
     [Parameter] public string ImageAlt { get; set; } = string.Empty;
+    [Parameter] public string CreditsUrl { get; set; } = string.Empty;
 
-    [Parameter] public int Xs { get; set; } = 6;
-    [Parameter] public int Sm { get; set; } = 3;
-    [Parameter] public int Md { get; set; } = 2;
+    [Parameter] public int Xs { get; set; } = 12;
+    [Parameter] public int Sm { get; set; } = 6;
+    [Parameter] public int Md { get; set; } = 4;
 }
