39c2ef94db7e352d642e3bd1058b306441c6fabd
Logan Devillard <logandvllrd@gmail.com>
2026-03-30 22:25:03 +0200

    added project cards

--- Components/ProjectCard.razor.cs
+++ Components/ProjectCard.razor.cs
@@ -5,8 +5,9 @@ namespace Portfolio.Components;
 public partial class ProjectCard : ComponentBase
 {
     [Parameter] public string Href { get; set; } = string.Empty;
+    [Parameter] public string GithubHref { get; set; } = string.Empty;
     [Parameter, EditorRequired] public string Title { get; set; } = string.Empty;
-    [Parameter] public string Category { get; set; } = string.Empty;
+    [Parameter] public IReadOnlyList<string> Categories { get; set; } = [];
     [Parameter, EditorRequired] public string Description { get; set; } = string.Empty;
     [Parameter, EditorRequired] public string ImageSrc { get; set; } = string.Empty;
 }
