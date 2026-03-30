afcaea2dde45f3cb34195753007af177fc9bcc4a
Logan Devillard <logandvllrd@gmail.com>
2026-03-30 21:20:50 +0200

    added component for articles

---
+++ Components/ArticleCard.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Components;
+
+public partial class ArticleCard : ComponentBase
+{
+    [Parameter, EditorRequired] public string Href { get; set; } = string.Empty;
+    [Parameter, EditorRequired] public string Title { get; set; } = string.Empty;
+    [Parameter] public string Category { get; set; } = string.Empty;
+    [Parameter, EditorRequired] public string ImageSrc { get; set; } = string.Empty;
+}
