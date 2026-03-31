81dce157e7b605e6fd6f16ee382682c923fb7193
Logan Devillard <logandvllrd@gmail.com>
2026-03-31 19:33:54 +0200

    added multiple categories support on ArticleCard component

--- Components/ArticleCard.razor.cs
+++ Components/ArticleCard.razor.cs
@@ -6,6 +6,6 @@ public partial class ArticleCard : ComponentBase
 {
     [Parameter, EditorRequired] public string Href { get; set; } = string.Empty;
     [Parameter, EditorRequired] public string Title { get; set; } = string.Empty;
-    [Parameter] public string Category { get; set; } = string.Empty;
+    [Parameter] public IReadOnlyList<string> Categories { get; set; } = [];
     [Parameter, EditorRequired] public string ImageSrc { get; set; } = string.Empty;
 }
