cf9daaa9c9281c49139cf51a3d3f4f558e02004d
ldevillard <logandvllrd@gmail.com>
2026-04-02 20:40:34 +0200

    added divider between sections on the home page

---
+++ Components/SectionDivider.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Components;
+
+public partial class SectionDivider : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string Label { get; set; } = string.Empty;
+
+    [Parameter]
+    public string? Caption { get; set; }
+}
